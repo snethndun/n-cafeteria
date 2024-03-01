562deed7c51fd5708cc2b7b2de7eae31d5e3cb7a
sachinNishalka <nishalkasachin@gmail.com>
2024-03-01 21:15:35 +0530

    compleated the front end of the loggin page

--- lib/Login_screen.dart
+++ lib/Login_screen.dart
@@ -1,55 +1,203 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:n_cafe/components/primary_max_button_style.dart';
 
-class loginScreen extends StatelessWidget {
+class loginScreen extends StatefulWidget {
   const loginScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return Stack(
-      alignment: Alignment.bottomCenter,
-      children: [
-        Image(
-          image: AssetImage("assets/images/Login_cover.png"),
-          fit: BoxFit.cover,
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-        ),
-        Padding(
-          padding: EdgeInsets.only(top: 350.0),
-          child: Container(
-            decoration: BoxDecoration(
-                color: Colors.white, borderRadius: BorderRadius.only(topRight: Radius.circular(25), topLeft: Radius.circular(25))),
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,
-                  children: [
-                    Row(
-                      children: [
-                        Text(
-                          "Welcome N-cafe!",
-                          style: TextStyle(
-                            color: Colors.black,
-                            fontFamily: "Poppins",
-                            fontSize: 30.0,
-                          ),
+  State<loginScreen> createState() => _loginScreenState();
+}
 
-                        ),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Text("Log into your account", style: TextStyle(fontSize: 20.0, fontFamily: "Poppins", color: Colors.grey),),
-                      ],
-                    )
-                  ],
+class _loginScreenState extends State<loginScreen> {
+  late bool _passwordVisible;
+
+  @override
+  void initState() {
+    _passwordVisible = false;
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/Login_cover.png'),
+                  fit: BoxFit.cover,
+                  alignment: Alignment(0, 1.2),
                 ),
               ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Color(0xFFF4F5F9),
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(25),
+                        topRight: Radius.circular(25.0),
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(25.0),
+                      child: Column(
+                        children: [
+                          Column(
+                            children: [
+                              Row(
+                                children: [
+                                  Text(
+                                    "Welcome N-Cafe!",
+                                    style: TextStyle(
+                                        fontFamily: 'Poppins-bold',
+                                        fontSize: 25.0),
+                                  ),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  Text(
+                                    "Log in to your account",
+                                    style: TextStyle(
+                                      fontFamily: 'Poppins',
+                                      fontSize: 15.0,
+                                      fontWeight: FontWeight.w500,
+                                      color: Color(0xFF868889),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              SizedBox(
+                                height: 25.0,
+                              ),
+                              Container(
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                    hintText: "Username",
+                                    hintStyle: TextStyle(
+                                        fontFamily: "Poppins",
+                                        fontSize: 15.0,
+                                        fontWeight: FontWeight.w400),
+                                    fillColor: Colors.white,
+                                    filled: true,
+                                    enabledBorder: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(6.0),
+                                      borderSide:
+                                          BorderSide(color: Colors.white),
+                                    ),
+                                    focusedBorder: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(6.0),
+                                      borderSide:
+                                          BorderSide(color: Colors.white),
+                                    ),
+                                    prefixIcon: Icon(Icons.person),
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 10.0,
+                              ),
+                              Container(
+                                child: TextField(
+                                  obscureText: !_passwordVisible,
+                                  decoration: InputDecoration(
+                                    hintText: "Password",
+                                    hintStyle: TextStyle(
+                                        fontFamily: "Poppins",
+                                        fontSize: 15.0,
+                                        fontWeight: FontWeight.w400),
+                                    fillColor: Colors.white,
+                                    filled: true,
+                                    enabledBorder: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(6.0),
+                                      borderSide:
+                                          BorderSide(color: Colors.white),
+                                    ),
+                                    focusedBorder: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(6.0),
+                                      borderSide:
+                                          BorderSide(color: Colors.white),
+                                    ),
+                                    prefixIcon: Icon(Icons.lock_outline),
+                                    suffixIcon: IconButton(
+                                      onPressed: () {
+                                        setState(() {
+                                          _passwordVisible = !_passwordVisible;
+                                        });
+                                      },
+                                      icon: Icon(_passwordVisible
+                                          ? Icons.visibility_outlined
+                                          : Icons.visibility_off_outlined),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                height: 10.0,
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                children: [
+                                  Text(
+                                    "Fogot Password",
+                                    style: TextStyle(
+                                        fontFamily: 'Poppins',
+                                        fontSize: 15.0,
+                                        fontWeight: FontWeight.w500,
+                                        color: Color(0xFF407EC7)),
+                                  ),
+                                ],
+                              ),
+                              SizedBox(
+                                height: 10.0,
+                              ),
+                              ElevatedButton(
+                                onPressed: () {},
+                                child: Text("Login"),
+                                style: PrimaryMaxButtonStyle(),
+                              ),
+                              SizedBox(height: 10.0),
+                              RichText(
+                                text: TextSpan(
+                                  children: [
+                                    TextSpan(
+                                      text: "Don`t have an account?",
+                                      style: TextStyle(
+                                          color: Color(0xFF868889),
+                                          fontFamily: 'Poppins',
+                                          fontSize: 15.0,
+                                          fontWeight: FontWeight.w500),
+                                    ),
+                                    TextSpan(
+                                      text: " Sign up",
+                                      style: TextStyle(
+                                          color: Colors.black,
+                                          fontFamily: 'Poppins',
+                                          fontSize: 15.0,
+                                          fontWeight: FontWeight.w500),
+                                    ),
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
-          ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }

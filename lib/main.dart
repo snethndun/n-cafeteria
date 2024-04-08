61c1e54247b369e0cffb606e68ca0c64dfbed5fd
sachinNishalka <nishalkasachin@gmail.com>
2024-04-08 23:25:07 +0530

    initialized the backend

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:n_cafe/screens/about_me.dart';
 import 'package:n_cafe/screens/added_review.dart';
@@ -12,7 +13,10 @@ import 'package:n_cafe/screens/search_screen.dart';
 import 'package:n_cafe/screens/sign_up_page.dart';
 import 'package:n_cafe/screens/verify_Email_Screen.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
